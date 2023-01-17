ffaabcba574681b360dc43d973c484b5537406fd
Francerz <francerzam@hotmail.com>
2023-01-17 10:16:08 -0600

    ADD periodo->inicio_clases y PeriodoCarrera

--- src/Model/Escolares/Periodo.php
+++ src/Model/Escolares/Periodo.php
@@ -2,28 +2,14 @@
 
 namespace ITColima\SiitecApi\Model\Escolares;
 
-use JsonSerializable;
-
-class Periodo implements JsonSerializable
+class Periodo
 {
     public $periodo_id;
     public $nombre;
     public $clave;
     public $inicio;
     public $termino;
+    public $inicio_clases;
     public $verano;
     public $actual;
-
-    public function jsonSerialize()
-    {
-        return [
-            'periodo_id' => $this->periodo_id,
-            'nombre' => $this->nombre,
-            'clave' => $this->clave,
-            'inicio' => $this->inicio,
-            'termino' => $this->termino,
-            'verano' => $this->verano,
-            'actual' => $this->actual
-        ];
-    }
 }
---
+++ src/Model/Escolares/PeriodoCarrera.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\Escolares;
+
+class PeriodoCarrera
+{
+    public $periodo_id;
+    public $carrera_id;
+    public $cargas_time;
+}
