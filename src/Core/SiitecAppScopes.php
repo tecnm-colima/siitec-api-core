6b44b3b936d18348bc1c30bee9ddd020973dcfa3
Francerz <francerzam@hotmail.com>
2023-02-23 13:33:34 -0600

    ADD usuario empleado post

--- src/Core/SiitecAppScopes.php
+++ src/Core/SiitecAppScopes.php
@@ -7,4 +7,5 @@ final class SiitecAppScopes
     public const POST_APP_EMAIL = 'app.email:post';
     public const POST_ESCOLARES_KARDEX = 'escolares.kardex:post';
     public const POST_USUARIO_ASPIRANTE = 'app.usuario.aspirante:post';
+    public const POST_USUARIO_EMPLEADO = 'app.usuario.empleado:post';
 }
