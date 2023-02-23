549404b252dc13359cb7394aa19487edc4265874
Francerz <francerzam@hotmail.com>
2023-02-23 13:30:01 -0600

    ADD modelo para Perfil y Empleado

---
+++ src/Model/App/Perfil.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\App;
+
+class Perfil
+{
+    public $perfil_id;
+    public $nombre;
+    public $descripcion;
+}
---
+++ src/Model/App/Usuarios/Empleado.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\App\Usuarios;
+
+class Empleado
+{
+    public $id_empleado;
+    public $id_usuario;
+    public $id_perfil;
+    public $nombres;
+    public $apellido1;
+    public $apellido2;
+    public $curp;
+    public $rfc;
+    public $usuario;
+    public $password;
+}
