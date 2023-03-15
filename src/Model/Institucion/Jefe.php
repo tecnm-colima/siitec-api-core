39650e85e979d1d3912eef755722ec7c396c4926
Francerz <francerzam@hotmail.com>
2023-03-15 12:54:08 -0600

    ADD /Models/Institucion/Jefe class

---
+++ src/Model/Institucion/Jefe.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\Institucion;
+
+class Jefe
+{
+    public $pk_id;
+    public $departamento_id;
+    public $empleado_id;
+    public $usuario_id;
+    public $departamento;
+    public $departamento_corto;
+    public $nombres;
+    public $apellido1;
+    public $apellido2;
+    public $sexo;
+    public $inicio;
+    public $fin;
+    public $activo;
+}
