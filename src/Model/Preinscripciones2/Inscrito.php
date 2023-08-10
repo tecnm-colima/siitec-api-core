d9f4d754e83d1eb3d8f8f548a5dfe2fd02d11184
Francerz <francerzam@hotmail.com>
2023-08-10 11:53:32 -0600

    ADD Model/Preinscripciones2/Inscrito

---
+++ src/Model/Preinscripciones2/Inscrito.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\Preinscripciones2;
+
+class Inscrito
+{
+    public $id_alumno;
+    public $id_aspirante;
+    public $id_carrera;
+    public $id_usuario_alumno;
+    public $id_usuario_aspirante;
+    public $num_control;
+    public $nombres;
+    public $apellido1;
+    public $apellido2;
+    public $curp;
+    public $pagado_time;
+}
