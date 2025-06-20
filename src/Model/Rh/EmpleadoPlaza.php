29d7a42d230022c95a0c8af7a877a5b7b23bbeea
Francerz <10836837+francerz@users.noreply.github.com>
2025-06-20 13:36:49 -0600

    ADD model EmpleadoPlaza

---
+++ src/Model/Rh/EmpleadoPlaza.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ITColima\SiitecApi\Model\Rh;
+
+/**
+ * @property-read string $clave_alta
+ * @property-read string $clave_categoria
+ * @property-read string $categoria
+ * @property-read string $fecha_inicio
+ * @property-read string $fecha_fin
+ */
+class EmpleadoPlaza
+{
+    public $empleado_id;
+    public $clavepago_id;
+}
